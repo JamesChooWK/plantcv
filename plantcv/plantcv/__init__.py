1d64650e970d3f2d953d539064d827da4ec86544
James <james@adinnovations.nl>
2019-12-20 09:09:42 +0100

    Updeted init file

--- plantcv/plantcv/__init__.py
+++ plantcv/plantcv/__init__.py
@@ -204,6 +204,9 @@ from plantcv.plantcv import morphology
 from plantcv.plantcv.fill_holes import fill_holes
 from plantcv.plantcv.get_kernel import get_kernel
 from plantcv.plantcv.crop import crop
+from plantcv.plantcv.warning import warning
+from plantcv.plantcv.peaks_analysis import peaks_analysis
+from plantcv.plantcv.plot_graph import plot_graph
 
 # add new functions to end of lists
 
@@ -220,7 +223,7 @@ __all__ = ['fatal_error', 'print_image', 'plot_image', 'color_palette', 'apply_m
            'cluster_contour_splitimg', 'rotate', 'shift_img', 'output_mask', 'auto_crop', 'canny_edge_detect',
            'background_subtraction', 'naive_bayes_classifier', 'acute', 'distance_transform', 'params',
            'cluster_contour_mask','analyze_thermal_values', 'opening',
-           'closing','within_frame', 'fill_holes', 'get_kernel', 'Spectral_data', 'crop']
+           'closing','within_frame', 'fill_holes', 'get_kernel', 'Spectral_data', 'crop', 'warning', 'peaks_analysis', 'plot_graph']
 
 from ._version import get_versions
 __version__ = get_versions()['version']
