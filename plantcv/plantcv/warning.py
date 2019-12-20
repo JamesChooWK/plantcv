5ade9d2861c1824997b934c09062d6050ac180c5
James <james@adinnovations.nl>
2019-12-20 09:13:37 +0100

    Printing out the warning message.

---
+++ plantcv/plantcv/warning.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Monday 16.12.2019
+Copyright (©) CHOO WAI KEONG (James) 
+Author: CHOO WAI KEONG (James) 
+Email: james@adinnovations.nl
+'''
+
+
+#--- Warning handling  ---
+
+def warning(warning):
+	""" Raise a warning message that passed.
+
+	input: warning message
+	:param warning: str
+
+	:return
+	"""
+
+	print(f" !!! {warning} !!!")
